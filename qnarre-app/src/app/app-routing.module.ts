dbe8f166ad04eeaab25a59eda912236f7a95309f
quantapix <quantapix@gmail.com>
2020-03-21 17:29:07 -0400

    qpx

--- qnarre-app/src/app/app-routing.module.ts
+++ qnarre-app/src/app/app-routing.module.ts
@@ -1,11 +1,42 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 
+import {ComposeMessageComponent} from './compose-message/compose-message.component';
+import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+import {AuthGuard} from './auth/auth.guard';
+import {PreloadService} from './preloading-strategy.service';
+
+const routes: Routes = [
+  {
+    path: 'compose',
+    component: ComposeMessageComponent,
+    outlet: 'popup'
+  },
+  {
+    path: 'admin',
+    loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule),
+    canLoad: [AuthGuard]
+  },
+  {
+    path: 'crisis-center',
+    loadChildren: () =>
+      import('./crisis-center/crisis-center.module').then(
+        m => m.CrisisCenterModule
+      ),
+    data: {preload: true}
+  },
+  {path: '', redirectTo: '/superheroes', pathMatch: 'full'},
+  {path: '**', component: PageNotFoundComponent}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes, {
+      preloadingStrategy: PreloadService,
+      enableTracing: false
+    })
+  ],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- qnarre-app/src/app/app.module.ts
+++ qnarre-app/src/app/app.module.ts
@@ -1,18 +1,57 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {Router} from '@angular/router';
+
+import {AppComponent} from './app.component';
+import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
+import {ComposeMessageComponent} from './compose-message/compose-message.component';
+
+import {AppRoutingModule} from './app-routing.module';
+
+import {AuthModule} from './auth/auth.module';
+import {HeroesModule} from './heroes/heroes.module';
+
+import {GraphComponent} from '../graph/graph.component';
+import {LinkComponent} from '../graph/link.component';
+import {NodeComponent} from '../graph/node.component';
+
+import {DraggableDirective} from '../graph/draggable.directive';
+import {ZoomableDirective} from '../graph/zoomable.directive';
+
+import {DataService} from '../graph/data.service';
+import {SceneService} from '../graph/scene.service';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HeroesModule,
+    HttpClientModule,
+    AuthModule,
     AppRoutingModule
   ],
-  providers: [],
+  declarations: [
+    AppComponent,
+    ComposeMessageComponent,
+    GraphComponent,
+    LinkComponent,
+    NodeComponent,
+    DraggableDirective,
+    ZoomableDirective,
+    PageNotFoundComponent
+  ],
+  providers: [DataService, SceneService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(_: Router) {
+    // Use a custom replacer to display function names in the route configs
+    // const replacer = (key, value) => (typeof value === 'function') ? value.name : value;
+    // console.log('Routes: ', JSON.stringify(router.config, replacer, 2));
+  }
+}
