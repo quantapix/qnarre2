eee48fab4e17d86456ae1bca71c9700bb0b25952
quantapix <quantapix@gmail.com>
2020-05-11 07:16:59 -0400

    qpx

--- proba/client/src/extension.ts
+++ proba/client/src/extension.ts
@@ -1,65 +1,45 @@
-/* --------------------------------------------------------------------------------------------
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License. See License.txt in the project root for license information.
- * ------------------------------------------------------------------------------------------ */
-
 import * as path from 'path';
-import { workspace, ExtensionContext } from 'vscode';
+import * as vscode from 'vscode';
 
 import {
-	LanguageClient,
-	LanguageClientOptions,
-	ServerOptions,
-	TransportKind
+  LanguageClient,
+  LanguageClientOptions,
+  ServerOptions,
+  TransportKind,
 } from 'vscode-languageclient';
 
 let client: LanguageClient;
 
-export function activate(context: ExtensionContext) {
-	// The server is implemented in node
-	let serverModule = context.asAbsolutePath(
-		path.join('server', 'out', 'server.js')
-	);
-	// The debug options for the server
-	// --inspect=6009: runs the server in Node's Inspector mode so VS Code can attach to the server for debugging
-	let debugOptions = { execArgv: ['--nolazy', '--inspect=6009'] };
+export function activate(ctx: vscode.ExtensionContext) {
+  console.log('Extension "proba" is now active!');
 
-	// If the extension is launched in debug mode then the debug server options are used
-	// Otherwise the run options are used
-	let serverOptions: ServerOptions = {
-		run: { module: serverModule, transport: TransportKind.ipc },
-		debug: {
-			module: serverModule,
-			transport: TransportKind.ipc,
-			options: debugOptions
-		}
-	};
+  const disposable = vscode.commands.registerCommand('extension.proba', () => {
+    vscode.window.showInformationMessage('Proba, proba, proba...');
+  });
+  ctx.subscriptions.push(disposable);
 
-	// Options to control the language client
-	let clientOptions: LanguageClientOptions = {
-		// Register the server for plain text documents
-		documentSelector: [{ scheme: 'file', language: 'plaintext' }],
-		synchronize: {
-			// Notify the server about file changes to '.clientrc files contained in the workspace
-			fileEvents: workspace.createFileSystemWatcher('**/.clientrc')
-		}
-	};
-
-	// Create the language client and start the client.
-	client = new LanguageClient(
-		'languageServerExample',
-		'Language Server Example',
-		serverOptions,
-		clientOptions
-	);
-
-	// Start the client. This will also launch the server
-	client.start();
+  let s = ctx.asAbsolutePath(path.join('server', 'out', 'server.js'));
+  const dOpts = { execArgv: ['--nolazy', '--inspect=6009'] };
+  const sOpts: ServerOptions = {
+    run: { module: s, transport: TransportKind.ipc },
+    debug: { module: s, transport: TransportKind.ipc, options: dOpts },
+  };
+  const cOpts: LanguageClientOptions = {
+    documentSelector: [{ scheme: 'file', language: 'plaintext' }],
+    synchronize: {
+      fileEvents: vscode.workspace.createFileSystemWatcher('**/.clientrc'),
+    },
+  };
+  client = new LanguageClient(
+    'languageServerExample',
+    'Language Server Example',
+    sOpts,
+    cOpts
+  );
+  client.start();
 }
 
 export function deactivate(): Thenable<void> | undefined {
-	if (!client) {
-		return undefined;
-	}
-	return client.stop();
+  if (client) return client.stop();
+  return;
 }
