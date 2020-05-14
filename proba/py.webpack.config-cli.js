e5cc8b42c95cd19949ca7d8f965203fc653c0440
quantapix <quantapix@gmail.com>
2020-05-14 11:21:59 -0400

    qpx

---
+++ proba/py.webpack.config-cli.js
@@ -0,0 +1,45 @@
+/**
+ * webpack.config-pyright.js
+ * Copyright: Microsoft 2018
+ *
+ * Configuration for webpack to bundle the javascript into a single file
+ * for the pyright command-line tool.
+ */
+
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+const path = require('path');
+
+module.exports = {
+    entry: './src/pyright.ts',
+    mode: 'development',
+    target: 'node',
+    output: {
+        filename: 'pyright.js',
+        path: path.resolve(__dirname, '../dist'),
+    },
+    resolve: {
+        modules: [path.resolve(__dirname, '.'), 'node_modules'],
+        extensions: ['.js', '.ts'],
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                loader: 'ts-loader',
+                options: {
+                    configFile: 'tsconfig.json',
+                },
+            },
+            {
+                test: /\.node$/,
+                loader: 'node-loader',
+            },
+        ],
+    },
+
+    node: {
+        fs: 'empty',
+    },
+};
---
+++ proba/py.webpack.config-server.js
@@ -0,0 +1,48 @@
+/**
+ * webpack.config-server.js
+ * Copyright: Microsoft 2018
+ *
+ * Configuration for webpack to bundle the javascript into a single file
+ * for the VS Code Extension language server.
+ */
+
+const path = require('path'); // eslint-disable-line @typescript-eslint/no-var-requires
+
+module.exports = {
+    context: path.resolve(__dirname),
+    entry: './src/server.ts',
+    mode: 'production',
+    target: 'node',
+    devtool: 'source-map',
+    output: {
+        filename: 'server.bundle.js',
+        path: path.resolve(__dirname, '../client/server'),
+    },
+    optimization: {
+        usedExports: true,
+    },
+    resolve: {
+        modules: [path.resolve(__dirname, '.'), 'node_modules'],
+        extensions: ['.js', '.ts'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                loader: 'ts-loader',
+                options: {
+                    configFile: 'tsconfig.json',
+                },
+            },
+            {
+                test: /\.node$/,
+                use: 'node-loader',
+            },
+        ],
+    },
+    node: {
+        fs: 'empty',
+        __dirname: false,
+        __filename: false,
+    },
+};
