46d25c91a549895699b320add40d6def9976fd19
quantapix <quantapix@gmail.com>
2020-05-24 18:02:17 -0400

    qpx

---
+++ proba/server/src-ts/compiler/builderStatePublic.ts
@@ -0,0 +1,12 @@
+export interface EmitOutput {
+  outputFiles: OutputFile[];
+  emitSkipped: boolean;
+  diagnostics: readonly Diagnostic[];
+  exportedModulesFromDeclarationEmit?: ExportedModulesFromDeclarationEmit;
+}
+
+export interface OutputFile {
+  name: string;
+  writeByteOrderMark: boolean;
+  text: string;
+}
