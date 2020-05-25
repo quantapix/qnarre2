0dcea070b7b95286c7077a1314bb5b6e8d59ed3d
quantapix <quantapix@gmail.com>
2020-05-25 09:42:45 -0400

    qpx

---
+++ proba/server/src-old/utils/diagnostic.ts
@@ -0,0 +1,260 @@
+import { Commands } from '../pyCommands';
+import { convertOffsetsToRange } from './position';
+import { Range, TextRange } from './text';
+import { TextRangeCollection } from './text';
+
+export const enum DiagnosticCategory {
+  Error,
+  Warning,
+  UnusedCode,
+}
+
+export interface DiagnosticAction {
+  action: string;
+}
+
+export interface CreateTypeStubFileAction extends DiagnosticAction {
+  action: Commands.createTypeStub;
+  moduleName: string;
+}
+
+export interface AddMissingOptionalToParamAction extends DiagnosticAction {
+  action: Commands.addMissingOptionalToParam;
+  offsetOfTypeNode: number;
+}
+
+export interface DiagnosticRelatedInfo {
+  message: string;
+  filePath: string;
+  range: Range;
+}
+
+export class Diagnostic {
+  private _actions: DiagnosticAction[] | undefined;
+  private _rule: string | undefined;
+  private _relatedInfo: DiagnosticRelatedInfo[] = [];
+
+  constructor(
+    readonly category: DiagnosticCategory,
+    readonly message: string,
+    readonly range: Range
+  ) {}
+
+  addAction(action: DiagnosticAction) {
+    if (this._actions === undefined) {
+      this._actions = [action];
+    } else {
+      this._actions.push(action);
+    }
+  }
+
+  getActions() {
+    return this._actions;
+  }
+
+  setRule(rule: string) {
+    this._rule = rule;
+  }
+
+  getRule() {
+    return this._rule;
+  }
+
+  addRelatedInfo(message: string, filePath: string, range: Range) {
+    this._relatedInfo.push({ filePath, message, range });
+  }
+
+  getRelatedInfo() {
+    return this._relatedInfo;
+  }
+}
+
+export class DiagnosticAddendum {
+  private _messages: string[] = [];
+  private _childAddenda: DiagnosticAddendum[] = [];
+
+  addMessage(message: string) {
+    this._messages.push(message);
+  }
+
+  createAddendum() {
+    const newAddendum = new DiagnosticAddendum();
+    this.addAddendum(newAddendum);
+    return newAddendum;
+  }
+
+  getString(maxDepth = 5, maxLineCount = 5): string {
+    let lines = this._getLinesRecursive(maxDepth);
+
+    if (lines.length > maxLineCount) {
+      lines = lines.slice(0, maxLineCount);
+      lines.push('...');
+    }
+
+    const text = lines.join('\n');
+    if (text.length > 0) {
+      return '\n' + text;
+    }
+
+    return '';
+  }
+
+  getMessageCount() {
+    return this._messages.length;
+  }
+
+  addAddendum(addendum: DiagnosticAddendum) {
+    this._childAddenda.push(addendum);
+  }
+
+  private _getLinesRecursive(maxDepth: number): string[] {
+    if (maxDepth <= 0) {
+      return [];
+    }
+    const childLines: string[] = [];
+    for (const addendum of this._childAddenda) {
+      childLines.push(...addendum._getLinesRecursive(maxDepth - 1));
+    }
+    const extraSpace = this._messages.length > 0 ? '  ' : '';
+    return this._messages.concat(childLines).map((line) => extraSpace + line);
+  }
+}
+
+export const enum DiagnosticRule {
+  strictListInference = 'strictListInference',
+  strictDictionaryInference = 'strictDictionaryInference',
+  strictParameterNoneValue = 'strictParameterNoneValue',
+  enableTypeIgnoreComments = 'enableTypeIgnoreComments',
+
+  reportGeneralTypeIssues = 'reportGeneralTypeIssues',
+  reportTypeshedErrors = 'reportTypeshedErrors',
+  reportMissingImports = 'reportMissingImports',
+  reportMissingModuleSource = 'reportMissingModuleSource',
+  reportMissingTypeStubs = 'reportMissingTypeStubs',
+  reportImportCycles = 'reportImportCycles',
+  reportUnusedImport = 'reportUnusedImport',
+  reportUnusedClass = 'reportUnusedClass',
+  reportUnusedFunction = 'reportUnusedFunction',
+  reportUnusedVariable = 'reportUnusedVariable',
+  reportDuplicateImport = 'reportDuplicateImport',
+  reportOptionalSubscript = 'reportOptionalSubscript',
+  reportOptionalMemberAccess = 'reportOptionalMemberAccess',
+  reportOptionalCall = 'reportOptionalCall',
+  reportOptionalIterable = 'reportOptionalIterable',
+  reportOptionalContextManager = 'reportOptionalContextManager',
+  reportOptionalOperand = 'reportOptionalOperand',
+  reportUntypedFunctionDecorator = 'reportUntypedFunctionDecorator',
+  reportUntypedClassDecorator = 'reportUntypedClassDecorator',
+  reportUntypedBaseClass = 'reportUntypedBaseClass',
+  reportUntypedNamedTuple = 'reportUntypedNamedTuple',
+  reportPrivateUsage = 'reportPrivateUsage',
+  reportConstantRedefinition = 'reportConstantRedefinition',
+  reportIncompatibleMethodOverride = 'reportIncompatibleMethodOverride',
+  reportInvalidStringEscapeSequence = 'reportInvalidStringEscapeSequence',
+  reportUnknownParameterType = 'reportUnknownParameterType',
+  reportUnknownArgumentType = 'reportUnknownArgumentType',
+  reportUnknownLambdaType = 'reportUnknownLambdaType',
+  reportUnknownVariableType = 'reportUnknownVariableType',
+  reportUnknownMemberType = 'reportUnknownMemberType',
+  reportCallInDefaultInitializer = 'reportCallInDefaultInitializer',
+  reportUnnecessaryIsInstance = 'reportUnnecessaryIsInstance',
+  reportUnnecessaryCast = 'reportUnnecessaryCast',
+  reportAssertAlwaysTrue = 'reportAssertAlwaysTrue',
+  reportSelfClsParameterName = 'reportSelfClsParameterName',
+  reportImplicitStringConcatenation = 'reportImplicitStringConcatenation',
+  reportUndefinedVariable = 'reportUndefinedVariable',
+  reportUnboundVariable = 'reportUnboundVariable',
+}
+
+export interface FileDiagnostics {
+  filePath: string;
+  diagnostics: Diagnostic[];
+}
+
+export class DiagnosticSink {
+  private _diagnosticList: Diagnostic[];
+  private _diagnosticMap: Map<string, Diagnostic>;
+
+  constructor(diagnostics?: Diagnostic[]) {
+    this._diagnosticList = diagnostics || [];
+    this._diagnosticMap = new Map<string, Diagnostic>();
+  }
+
+  fetchAndClear() {
+    const prevDiagnostics = this._diagnosticList;
+    this._diagnosticList = [];
+    this._diagnosticMap.clear();
+    return prevDiagnostics;
+  }
+
+  addError(message: string, range: Range) {
+    return this.addDiagnostic(new Diagnostic(DiagnosticCategory.Error, message, range));
+  }
+
+  addWarning(message: string, range: Range) {
+    return this.addDiagnostic(new Diagnostic(DiagnosticCategory.Warning, message, range));
+  }
+
+  addUnusedCode(message: string, range: Range) {
+    return this.addDiagnostic(
+      new Diagnostic(DiagnosticCategory.UnusedCode, message, range)
+    );
+  }
+
+  addDiagnostic(diag: Diagnostic) {
+    const key =
+      `${diag.range.start.line},${diag.range.start.character}-` +
+      `${diag.range.end.line}-${diag.range.end.character}:${diag.message.substr(0, 25)}}`;
+    if (!this._diagnosticMap.has(key)) {
+      this._diagnosticList.push(diag);
+      this._diagnosticMap.set(key, diag);
+    }
+    return diag;
+  }
+
+  addDiagnostics(diagsToAdd: Diagnostic[]) {
+    this._diagnosticList.push(...diagsToAdd);
+  }
+
+  getErrors() {
+    return this._diagnosticList.filter(
+      (diag) => diag.category === DiagnosticCategory.Error
+    );
+  }
+
+  getWarnings() {
+    return this._diagnosticList.filter(
+      (diag) => diag.category === DiagnosticCategory.Warning
+    );
+  }
+}
+
+export class TextRangeDiagnosticSink extends DiagnosticSink {
+  private _lines: TextRangeCollection<TextRange>;
+
+  constructor(lines: TextRangeCollection<TextRange>, diagnostics?: Diagnostic[]) {
+    super(diagnostics);
+    this._lines = lines;
+  }
+
+  addErrorWithTextRange(message: string, range: TextRange) {
+    return this.addError(
+      message,
+      convertOffsetsToRange(range.start, range.start + range.length, this._lines)
+    );
+  }
+
+  addWarningWithTextRange(message: string, range: TextRange) {
+    return this.addWarning(
+      message,
+      convertOffsetsToRange(range.start, range.start + range.length, this._lines)
+    );
+  }
+
+  addUnusedCodeWithTextRange(message: string, range: TextRange) {
+    return this.addUnusedCode(
+      message,
+      convertOffsetsToRange(range.start, range.start + range.length, this._lines)
+    );
+  }
+}
--- proba/server/src/utils/diagnostic.ts
+++ proba/server/src/utils/diagnostic.ts
@@ -1,260 +1,211 @@
-import { Commands } from '../pyCommands';
-import { convertOffsetsToRange } from './position';
-import { Range, TextRange } from './text';
-import { TextRangeCollection } from './text';
+import * as vsc from 'vscode';
 
-export const enum DiagnosticCategory {
-  Error,
-  Warning,
-  UnusedCode,
+import * as qu from '.';
+import * as qx from './extras';
+
+export const enum Kind {
+  Syntax,
+  Semantic,
+  Suggestion,
 }
 
-export interface DiagnosticAction {
-  action: string;
+export const enum Lang {
+  JavaScript,
+  TypeScript,
 }
 
-export interface CreateTypeStubFileAction extends DiagnosticAction {
-  action: Commands.createTypeStub;
-  moduleName: string;
+export const allLangs = [Lang.JavaScript, Lang.TypeScript];
+
+class FileDiags {
+  private readonly map = new Map<Kind, ReadonlyArray<vsc.Diagnostic>>();
+
+  constructor(public readonly file: vsc.Uri, public lang: Lang) {}
+
+  update(l: Lang, k: Kind, ds: ReadonlyArray<vsc.Diagnostic>) {
+    if (l !== this.lang) {
+      this.map.clear();
+      this.lang = l;
+    }
+    const d = this.map.get(k);
+    if (qu.deepEquals(d || qu.empty, ds, equals)) return false;
+    this.map.set(k, ds);
+    return true;
+  }
+
+  diagnostics(s: Settings) {
+    if (!s.validate(this.lang)) return [];
+    return [...this.get(Kind.Syntax), ...this.get(Kind.Semantic), ...this.suggestions(s)];
+  }
+
+  private suggestions(s: Settings) {
+    const enable = s.suggestions(this.lang);
+    return this.get(Kind.Suggestion).filter((d) => {
+      if (!enable) return d.tags && d.tags.includes(vsc.DiagnosticTag.Unnecessary);
+      return true;
+    });
+  }
+
+  private get(k: Kind) {
+    return this.map.get(k) || [];
+  }
 }
 
-export interface AddMissingOptionalToParamAction extends DiagnosticAction {
-  action: Commands.addMissingOptionalToParam;
-  offsetOfTypeNode: number;
+function equals(a: vsc.Diagnostic, b: vsc.Diagnostic) {
+  if (a === b) return true;
+  return (
+    a.code === b.code &&
+    a.message === b.message &&
+    a.severity === b.severity &&
+    a.source === b.source &&
+    a.range.isEqual(b.range) &&
+    qu.deepEquals(
+      a.relatedInformation || qu.empty,
+      b.relatedInformation || qu.empty,
+      (a, b) => {
+        return (
+          a.message === b.message &&
+          a.location.range.isEqual(b.location.range) &&
+          a.location.uri.fsPath === b.location.uri.fsPath
+        );
+      }
+    ) &&
+    qu.equals(a.tags || qu.empty, b.tags || qu.empty)
+  );
 }
 
-export interface DiagnosticRelatedInfo {
-  message: string;
-  filePath: string;
-  range: Range;
+interface LangSettings {
+  readonly validate: boolean;
+  readonly suggestions: boolean;
 }
 
-export class Diagnostic {
-  private _actions: DiagnosticAction[] | undefined;
-  private _rule: string | undefined;
-  private _relatedInfo: DiagnosticRelatedInfo[] = [];
+function settingsEqual(s: LangSettings, n: LangSettings) {
+  return s.validate === n.validate && s.suggestions == n.suggestions;
+}
 
-  constructor(
-    readonly category: DiagnosticCategory,
-    readonly message: string,
-    readonly range: Range
-  ) {}
+class Settings {
+  private static readonly default: LangSettings = {
+    validate: true,
+    suggestions: true,
+  };
 
-  addAction(action: DiagnosticAction) {
-    if (this._actions === undefined) {
-      this._actions = [action];
-    } else {
-      this._actions.push(action);
+  private readonly map = new Map<Lang, LangSettings>();
+
+  validate(l: Lang) {
+    return this.get(l).validate;
+  }
+
+  setValidate(l: Lang, v: boolean) {
+    return this.update(l, (s) => ({
+      validate: v,
+      suggestions: s.suggestions,
+    }));
+  }
+
+  suggestions(l: Lang) {
+    return this.get(l).suggestions;
+  }
+
+  setSuggestions(l: Lang, v: boolean) {
+    return this.update(l, (s) => ({
+      validate: s.validate,
+      suggestions: v,
+    }));
+  }
+
+  private get(l: Lang) {
+    return this.map.get(l) ?? Settings.default;
+  }
+
+  private update(l: Lang, f: (_: LangSettings) => LangSettings) {
+    const s = this.get(l);
+    const n = f(s);
+    this.map.set(l, n);
+    return settingsEqual(s, n);
+  }
+}
+
+export class Diags extends qx.Disposable {
+  private readonly diags = new qx.ResourceMap<FileDiags>();
+  private readonly settings = new Settings();
+  private readonly current: vsc.DiagnosticCollection;
+  private readonly pending = new qx.ResourceMap<any>();
+  private readonly delay = 50;
+
+  constructor(owner: string) {
+    super();
+    this.current = this.register(vsc.languages.createDiagnosticCollection(owner));
+  }
+
+  dispose() {
+    super.dispose();
+    for (const v of this.pending.values) {
+      clearTimeout(v);
+    }
+    this.pending.clear();
+  }
+
+  reInitialize() {
+    this.current.clear();
+    this.diags.clear();
+  }
+
+  setValidate(l: Lang, v: boolean) {
+    if (this.settings.setValidate(l, v)) this.rebuild();
+  }
+
+  setSuggestions(l: Lang, v: boolean) {
+    if (this.settings.setSuggestions(l, v)) this.rebuild();
+  }
+
+  update(r: vsc.Uri, l: Lang, k: Kind, ds: ReadonlyArray<vsc.Diagnostic>) {
+    let dirty = false;
+    const d = this.diags.get(r);
+    if (d) {
+      dirty = d.update(l, k, ds);
+    } else if (ds.length) {
+      const d = new FileDiags(r, l);
+      d.update(l, k, ds);
+      this.diags.set(r, d);
+      dirty = true;
+    }
+    if (dirty) this.scheduleUpdate(r);
+  }
+
+  configFileDiagsReceived(r: vsc.Uri, ds: ReadonlyArray<vsc.Diagnostic>) {
+    this.current.set(r, ds);
+  }
+
+  delete(r: vsc.Uri) {
+    this.current.delete(r);
+    this.diags.delete(r);
+  }
+
+  diagnostics(r: vsc.Uri) {
+    return this.current.get(r) ?? [];
+  }
+
+  private scheduleUpdate(r: vsc.Uri) {
+    if (!this.pending.has(r)) {
+      this.pending.set(
+        r,
+        setTimeout(() => this.updateCurrentDiagnostics(r), this.delay)
+      );
     }
   }
 
-  getActions() {
-    return this._actions;
-  }
-
-  setRule(rule: string) {
-    this._rule = rule;
-  }
-
-  getRule() {
-    return this._rule;
-  }
-
-  addRelatedInfo(message: string, filePath: string, range: Range) {
-    this._relatedInfo.push({ filePath, message, range });
-  }
-
-  getRelatedInfo() {
-    return this._relatedInfo;
-  }
-}
-
-export class DiagnosticAddendum {
-  private _messages: string[] = [];
-  private _childAddenda: DiagnosticAddendum[] = [];
-
-  addMessage(message: string) {
-    this._messages.push(message);
-  }
-
-  createAddendum() {
-    const newAddendum = new DiagnosticAddendum();
-    this.addAddendum(newAddendum);
-    return newAddendum;
-  }
-
-  getString(maxDepth = 5, maxLineCount = 5): string {
-    let lines = this._getLinesRecursive(maxDepth);
-
-    if (lines.length > maxLineCount) {
-      lines = lines.slice(0, maxLineCount);
-      lines.push('...');
+  private updateCurrentDiagnostics(r: vsc.Uri) {
+    if (this.pending.has(r)) {
+      clearTimeout(this.pending.get(r));
+      this.pending.delete(r);
     }
+    const d = this.diags.get(r);
+    this.current.set(r, d ? d.diagnostics(this.settings) : []);
+  }
 
-    const text = lines.join('\n');
-    if (text.length > 0) {
-      return '\n' + text;
+  private rebuild() {
+    this.current.clear();
+    for (const d of this.diags.values) {
+      this.current.set(d.file, d.diagnostics(this.settings));
     }
-
-    return '';
-  }
-
-  getMessageCount() {
-    return this._messages.length;
-  }
-
-  addAddendum(addendum: DiagnosticAddendum) {
-    this._childAddenda.push(addendum);
-  }
-
-  private _getLinesRecursive(maxDepth: number): string[] {
-    if (maxDepth <= 0) {
-      return [];
-    }
-    const childLines: string[] = [];
-    for (const addendum of this._childAddenda) {
-      childLines.push(...addendum._getLinesRecursive(maxDepth - 1));
-    }
-    const extraSpace = this._messages.length > 0 ? '  ' : '';
-    return this._messages.concat(childLines).map((line) => extraSpace + line);
-  }
-}
-
-export const enum DiagnosticRule {
-  strictListInference = 'strictListInference',
-  strictDictionaryInference = 'strictDictionaryInference',
-  strictParameterNoneValue = 'strictParameterNoneValue',
-  enableTypeIgnoreComments = 'enableTypeIgnoreComments',
-
-  reportGeneralTypeIssues = 'reportGeneralTypeIssues',
-  reportTypeshedErrors = 'reportTypeshedErrors',
-  reportMissingImports = 'reportMissingImports',
-  reportMissingModuleSource = 'reportMissingModuleSource',
-  reportMissingTypeStubs = 'reportMissingTypeStubs',
-  reportImportCycles = 'reportImportCycles',
-  reportUnusedImport = 'reportUnusedImport',
-  reportUnusedClass = 'reportUnusedClass',
-  reportUnusedFunction = 'reportUnusedFunction',
-  reportUnusedVariable = 'reportUnusedVariable',
-  reportDuplicateImport = 'reportDuplicateImport',
-  reportOptionalSubscript = 'reportOptionalSubscript',
-  reportOptionalMemberAccess = 'reportOptionalMemberAccess',
-  reportOptionalCall = 'reportOptionalCall',
-  reportOptionalIterable = 'reportOptionalIterable',
-  reportOptionalContextManager = 'reportOptionalContextManager',
-  reportOptionalOperand = 'reportOptionalOperand',
-  reportUntypedFunctionDecorator = 'reportUntypedFunctionDecorator',
-  reportUntypedClassDecorator = 'reportUntypedClassDecorator',
-  reportUntypedBaseClass = 'reportUntypedBaseClass',
-  reportUntypedNamedTuple = 'reportUntypedNamedTuple',
-  reportPrivateUsage = 'reportPrivateUsage',
-  reportConstantRedefinition = 'reportConstantRedefinition',
-  reportIncompatibleMethodOverride = 'reportIncompatibleMethodOverride',
-  reportInvalidStringEscapeSequence = 'reportInvalidStringEscapeSequence',
-  reportUnknownParameterType = 'reportUnknownParameterType',
-  reportUnknownArgumentType = 'reportUnknownArgumentType',
-  reportUnknownLambdaType = 'reportUnknownLambdaType',
-  reportUnknownVariableType = 'reportUnknownVariableType',
-  reportUnknownMemberType = 'reportUnknownMemberType',
-  reportCallInDefaultInitializer = 'reportCallInDefaultInitializer',
-  reportUnnecessaryIsInstance = 'reportUnnecessaryIsInstance',
-  reportUnnecessaryCast = 'reportUnnecessaryCast',
-  reportAssertAlwaysTrue = 'reportAssertAlwaysTrue',
-  reportSelfClsParameterName = 'reportSelfClsParameterName',
-  reportImplicitStringConcatenation = 'reportImplicitStringConcatenation',
-  reportUndefinedVariable = 'reportUndefinedVariable',
-  reportUnboundVariable = 'reportUnboundVariable',
-}
-
-export interface FileDiagnostics {
-  filePath: string;
-  diagnostics: Diagnostic[];
-}
-
-export class DiagnosticSink {
-  private _diagnosticList: Diagnostic[];
-  private _diagnosticMap: Map<string, Diagnostic>;
-
-  constructor(diagnostics?: Diagnostic[]) {
-    this._diagnosticList = diagnostics || [];
-    this._diagnosticMap = new Map<string, Diagnostic>();
-  }
-
-  fetchAndClear() {
-    const prevDiagnostics = this._diagnosticList;
-    this._diagnosticList = [];
-    this._diagnosticMap.clear();
-    return prevDiagnostics;
-  }
-
-  addError(message: string, range: Range) {
-    return this.addDiagnostic(new Diagnostic(DiagnosticCategory.Error, message, range));
-  }
-
-  addWarning(message: string, range: Range) {
-    return this.addDiagnostic(new Diagnostic(DiagnosticCategory.Warning, message, range));
-  }
-
-  addUnusedCode(message: string, range: Range) {
-    return this.addDiagnostic(
-      new Diagnostic(DiagnosticCategory.UnusedCode, message, range)
-    );
-  }
-
-  addDiagnostic(diag: Diagnostic) {
-    const key =
-      `${diag.range.start.line},${diag.range.start.character}-` +
-      `${diag.range.end.line}-${diag.range.end.character}:${diag.message.substr(0, 25)}}`;
-    if (!this._diagnosticMap.has(key)) {
-      this._diagnosticList.push(diag);
-      this._diagnosticMap.set(key, diag);
-    }
-    return diag;
-  }
-
-  addDiagnostics(diagsToAdd: Diagnostic[]) {
-    this._diagnosticList.push(...diagsToAdd);
-  }
-
-  getErrors() {
-    return this._diagnosticList.filter(
-      (diag) => diag.category === DiagnosticCategory.Error
-    );
-  }
-
-  getWarnings() {
-    return this._diagnosticList.filter(
-      (diag) => diag.category === DiagnosticCategory.Warning
-    );
-  }
-}
-
-export class TextRangeDiagnosticSink extends DiagnosticSink {
-  private _lines: TextRangeCollection<TextRange>;
-
-  constructor(lines: TextRangeCollection<TextRange>, diagnostics?: Diagnostic[]) {
-    super(diagnostics);
-    this._lines = lines;
-  }
-
-  addErrorWithTextRange(message: string, range: TextRange) {
-    return this.addError(
-      message,
-      convertOffsetsToRange(range.start, range.start + range.length, this._lines)
-    );
-  }
-
-  addWarningWithTextRange(message: string, range: TextRange) {
-    return this.addWarning(
-      message,
-      convertOffsetsToRange(range.start, range.start + range.length, this._lines)
-    );
-  }
-
-  addUnusedCodeWithTextRange(message: string, range: TextRange) {
-    return this.addUnusedCode(
-      message,
-      convertOffsetsToRange(range.start, range.start + range.length, this._lines)
-    );
   }
 }
